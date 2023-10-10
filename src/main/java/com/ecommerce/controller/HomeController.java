34aecf1a8bf7e7e21b31e51376c38b2c08d25062
Franco Maurino <francomaurino07@gmail.com>
2023-10-10 04:15:26 -0300

    mostrar producto en detalle

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.ecommerce.controller;
 
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Producto;
 import com.ecommerce.service.ProductoService;
 
 @Controller
@@ -27,8 +31,16 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productHome(@PathVariable Integer id) {
+	public String productHome(@PathVariable Integer id, Model model) {
 		LOGGER.info("Id producto enviado como parametro{}",id);
+		
+		Producto producto = new Producto();
+		
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
 		return "usuario/productohome";
 	}
 	
