250ff6231baa6e51fbb70f27a31ed7502afb3cc1
Franco Maurino <francomaurino07@gmail.com>
2023-10-13 01:58:56 -0300

    metodo generar numero orden

--- src/main/java/com/ecommerce/service/DetalleOrdenServiceImpl.java
+++ src/main/java/com/ecommerce/service/DetalleOrdenServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ecommerce.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.model.DetalleOrden;
+import com.ecommerce.model.Orden;
 import com.ecommerce.repository.IDetalleOrdenRepository;
 
 @Service
@@ -14,9 +17,9 @@ public class DetalleOrdenServiceImpl implements IDetalleOrdenService {
 	private IDetalleOrdenRepository detalleOrdenRepository;
 	
 	@Override
-	public DetalleOrden save(DetalleOrden detalleOrden) {
-		
+	public DetalleOrden save(DetalleOrden detalleOrden) {		
 		return detalleOrdenRepository.save(detalleOrden);
 	}
+
 
 }
--- src/main/java/com/ecommerce/service/IDetalleOrdenService.java
+++ src/main/java/com/ecommerce/service/IDetalleOrdenService.java
@@ -1,7 +1,9 @@
 package com.ecommerce.service;
 
+import java.util.List;
+
 import com.ecommerce.model.DetalleOrden;
 
 public interface IDetalleOrdenService {
-	DetalleOrden save (DetalleOrden detalleOrden);
+	DetalleOrden save (DetalleOrden detalleOrden);	
 }
