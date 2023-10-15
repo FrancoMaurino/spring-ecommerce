653b50621cc060a838968fc92417abbbb4e8d30f
Franco Maurino <francomaurino07@gmail.com>
2023-10-15 00:22:46 -0300

    guardar usuarios

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -1,15 +1,21 @@
 package com.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Usuario;
 import com.ecommerce.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(UsuarioController.class);
 	
 	@Autowired
 	private IUsuarioService usuarioService;
@@ -19,4 +25,16 @@ public class UsuarioController {
 	public String create() {
 		return "usuario/registro";
 	}
+	
+	@PostMapping("/save")
+	public String save(Usuario usuario) {
+		LOGGER.info("Usuario registro: {}", usuario);
+		usuario.setTipo("USER");
+		usuarioService.save(usuario);
+		
+		
+		return "redirect:/";
+	}
+	
+	
 }
