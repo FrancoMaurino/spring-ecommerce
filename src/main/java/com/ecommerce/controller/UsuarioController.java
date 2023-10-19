d6f035e44136282d081a5fb8311d51e935e7dfc8
Franco Maurino <francomaurino07@gmail.com>
2023-10-19 00:22:56 -0300

    se agrega vista compras

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.model.Usuario;
 import com.ecommerce.service.IUsuarioService;
+
 
 import jakarta.servlet.http.HttpSession;
 
@@ -64,7 +66,19 @@ public class UsuarioController {
 	}
 	
 	
-	
+	@GetMapping("/compras")
+	public String obtenerCompras(HttpSession session, Model model) {
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "usuario/compras";
+	}
 	
 	
 }
+
+
+
+
+
+
+
+
