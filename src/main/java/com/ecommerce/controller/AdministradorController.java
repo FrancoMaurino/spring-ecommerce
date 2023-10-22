bde5a4f81f8e760b2e4f70aa926451284c60f6ed
Franco Maurino <francomaurino07@gmail.com>
2023-10-22 01:16:55 -0300

    ver detalles orden para administrador

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -2,12 +2,16 @@ package com.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
 import com.ecommerce.service.IOrdenService;
 import com.ecommerce.service.IUsuarioService;
@@ -27,6 +31,8 @@ public class AdministradorController {
 	
 	@Autowired
 	private IOrdenService ordenService;
+	
+	private Logger LOGGER = LoggerFactory.getLogger(AdministradorController.class);
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -50,4 +56,20 @@ public class AdministradorController {
 		return "administrador/ordenes";
 	}
 	
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id) {
+		LOGGER.info("id de la orden: {}", id);
+		Orden orden = ordenService.findById(id).get();
+		
+		model.addAttribute("detalles", orden.getDetalle());
+		
+		return "administrador/detalleorden";
+		
+	}
+	
+	
+	
 }
+
+
+
