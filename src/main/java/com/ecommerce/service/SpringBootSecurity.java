2b867f446a33dda700887942c735240c21546a4f
Franco Maurino <francomaurino07@gmail.com>
2023-10-30 23:48:42 -0300

    pruebas springSecurity 6

--- src/main/java/com/ecommerce/service/SpringBootSecurity.java
+++ src/main/java/com/ecommerce/service/SpringBootSecurity.java
@@ -2,39 +2,71 @@ package com.ecommerce.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.RememberMeServices;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSecurity
-public class SpringBootSecurity {
+public class SpringBootSecurity implements WebMvcConfigurer {
 
-	  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-	        http
-	            .authorizeRequests(authorize -> authorize
-	                .requestMatchers("/administrador/**").hasRole("ADMIN")
-	                .requestMatchers("/productos/**").hasRole("ADMIN")
-	                .requestMatchers("/usuario/login").permitAll()
-	                .requestMatchers().authenticated()
-	            )
-	            .formLogin()
-	            .loginPage("/usuario/login")
-	            .defaultSuccessUrl("/usuario/acceder")
-	            .and()
-	            .rememberMe(remember -> remember
-	                .rememberMeServices(rememberMeServices())
-	            )
-	            .csrf(csrf -> csrf
-	                .csrfTokenRepository(csrfTokenRepository())
-	            );
-
-	        return http.build();
-	    }
+	@Autowired
+	private UserDetailsService userDetailService;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+             
+         http.csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> {
+                request.requestMatchers("/usuario/login").permitAll();
+                request.requestMatchers("/administrador/**").hasAuthority( "ADMIN");
+                request.requestMatchers("/productos/**").hasAuthority( "ADMIN");
+            })
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/usuario/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/usuario/acceder"));               
+                
+         http.cors(withDefaults());
+     return http.build();   
+         
+    }
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+	    return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	/*
+	 * protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	 * {
+	 * auth.userDetailsService(userDetailsService()).passwordEncoder(getEnecoder());
+	 * }
+	 */
+	
 
 	@Bean
 	public RememberMeServices rememberMeServices() {
@@ -47,10 +79,5 @@ public class SpringBootSecurity {
 		// Return a custom UserDetailsService
 		return new UserDetailServiceImpl(); // Replace with your custom implementation
 	}
-
-	@Bean
-	public CsrfTokenRepository csrfTokenRepository() {
-		CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-		return csrfTokenRepository;
-	}
+	
 }
